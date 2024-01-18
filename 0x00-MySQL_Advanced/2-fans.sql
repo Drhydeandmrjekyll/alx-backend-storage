15465f3117ee49c1df8bb5ef7863aca57441738a
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-18 18:50:14 +0000

    2_nb

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,16 +2,20 @@
 
 -- Create a temporary table to store the aggregated fan counts for each origin
 CREATE TEMPORARY TABLE IF NOT EXISTS tmp_fan_counts AS
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-GROUP BY origin;
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
+-- Initialize variables for ranking
+SET @rank = 0;
+SET @prev_nb_fans = NULL;
 
 -- Rank the origins based on the number of non-unique fans
-SET @rank = 0;
 SELECT origin, nb_fans,
-       @rank := @rank + 1 AS rank
-FROM tmp_fan_counts
-ORDER BY nb_fans DESC;
+       @rank := IF(@prev_nb_fans = nb_fans, @rank, @rank + 1) AS rank,
+       @prev_nb_fans := nb_fans
+FROM tmp_fan_counts;
 
 -- Drop the temporary table after use
 DROP TEMPORARY TABLE IF EXISTS tmp_fan_counts;
