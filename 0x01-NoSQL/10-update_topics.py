62fb087bd52038630194dec18587c4c6f2c427b4
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-23 02:22:40 -0800

    10_update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+10-update_topics module
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of school document based on name."""
+    filter_query = {"name": name}
+    update_query = {"$set": {"topics": topics}}
+    result = mongo_collection.update_many(filter_query, update_query)
+    return result.modified_count
